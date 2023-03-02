a13f4c595a360b8d14faaf3c2812a9838f7718ce
marcoguiidi <123477203+marcoguiidi@users.noreply.github.com>
2023-03-02 18:46:40 +0100

    .

--- strutture.h
+++ strutture.h
@@ -4,7 +4,7 @@
 #include <ncurses.h>
 
 // file in cui mettere le strutture dati per essere usate in tutti i file
-//include anche le librerie così da non doverle mettere in ogni file, basta includere questo file
+//include anche le librerie così da non doverle mettere in ogni file, basta includere questo
 
 
 
