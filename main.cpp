9976db9b6782c17c1d794731762c826a0535b885
marcoguiidi <marco.guidi24@studio.unibo.it>
2023-08-24 12:09:10 +0200

    prima mappa creata

--- main.cpp
+++ main.cpp
@@ -26,42 +26,32 @@ int main() {
     refresh();
     wrefresh(playwin);
 
-    mapList* MM;
+    // creo mapList
+    mapList *MM;
 
     Protagonista *p = new Protagonista(MM, 1, 1, '@', 1000, 10, A, 2);
-    Goblin *g = new Goblin(30, 50, MM->getMap().win, 10, 40, 10, p);
-    Arciere *a = new Arciere(20, 10, MM->getMap().win, 20, 40, 10, p);
+
+    MM = new mapList(0, 0, p);
+
+    //creo mappa
+    mvwprintw(playwin, 15, 15, "|");
+
+    map M = createMap(playwin);
+
+    // aggiungo mappa alla lista
+    MM->add(M);
+
+    //aggiungo nemici alla mappa
+
+    MM->addArch(10, 20, 20, 40, 200);
+    MM->addGob(100, 5, 20, 30, 40);
+
+
+
+
     mvwprintw(stdscr, 10, 60, "Key arrows to move, S-shooting, C-change weapon");
     mvwprintw(stdscr, 11, 60, "Ctrl C to close");
-    auto startTime = chrono::steady_clock::now();
-    usleep(1000000);
-    do
-    {
-        p->display();
-        g->display();
-        a->display();
 
-        int choice = p->getmv();
-        if(choice == 's')
-        {
-           // g->checkDamage();
-        }
-
-        wrefresh(playwin);
-        refresh();
-        if (chrono::duration_cast<chrono::nanoseconds>(chrono::steady_clock::now() - startTime).count() % 20000 == 0)
-        {
-            g->getmv();
-            g->display();
-            wrefresh(playwin);
-        }
-
-        g->display();
-        wrefresh(playwin);
-        refresh();
-
-    }
-    while (true);   // termina con ctrl C
 
     endwin();
 
